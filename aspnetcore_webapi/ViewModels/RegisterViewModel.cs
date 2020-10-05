8fac9e82dd7ba4b104d3019c84490a1aa2390544
Anthony Hurtado <amhurtado@protonmail.com>
2020-10-05 14:12:45 -0500

    Add project files.

---
+++ aspnetcore_webapi/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcore_webapi.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ aspnetcore_webapi/ViewModels/ResponseStatusViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcore_webapi.ViewModels
+{
+    public class ResponseStatusViewModel
+    {
+        public bool Result { get; set; }
+        public List<string> Messages { get; set; }
+
+        public ResponseStatusViewModel()
+        {
+            Messages = new List<string>();
+        }
+    }
+}
